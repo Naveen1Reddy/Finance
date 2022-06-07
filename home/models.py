9bd853e2391d921db26fd2e11b8eb90033e0122e
Naveen Reddy <maarthanaveen228@gmail.com>
2022-06-07 15:34:46 +0530

    Added admin model and Created admin login page

--- home/models.py
+++ home/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+TYPE = ( 
+
+    ('positive' , 'Positive'),
+    ('negative' , 'Negative')
+)
+
+class profile(models.Model):
+    user = models.ForeignKey(User , on_delete = models.CASCADE)
+    income = models.FloatField()
+    balance = models.FloatField(null= True)
+
+
+class Expense(models.Model):
+    name = models.CharField(max_length= 100)
+    expense_type = models.CharField(max_length=  100, choices= TYPE)
+
+    def __str__(self):
+        return self.name
+
+    
