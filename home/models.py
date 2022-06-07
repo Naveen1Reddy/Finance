1b9c1880f3f26da78eccf42df7da7605eb3c6c72
Naveen Reddy <maarthanaveen228@gmail.com>
2022-06-07 18:02:52 +0530

    Added Functionality to frontend for balance,expense,income Tracking

--- home/models.py
+++ home/models.py
@@ -16,7 +16,9 @@ class Profile(models.Model):
 
 
 class Expense(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
     name = models.CharField(max_length= 100)
+    amount = models.FloatField(default=0, null= True)
     expense_type = models.CharField(max_length=  100, choices= TYPE)
 
     def __str__(self):
--- home/views.py
+++ home/views.py
@@ -1,19 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 
 # Create your views here.
 
 def home(request):
+    profile = Profile.objects.filter(user = request.user).first()
+    expenses = Expense.objects.filter(user = request.user)
+
     if request.method == "POST":
         text = request.POST.get("text")
-        amount = request.POST.get('amount')
+        amount = request.POST.get("amount")
         expense_type = request.POST.get("expense_type")
 
-        expense = Expense(name = text , amount = amount , expense_type = expense_type)
+        expense = Expense(name = text , amount = amount , expense_type = expense_type,user = request.user)
         expense.save()
 
+        if expense_type == "Positive":
+            profile.balance += float(amount)
 
-    profile = Profile.objects.filter(user = request.user).first()
-    context = {"profile" : profile}
+        else:
+            profile.expenses += float(amount)
+            profile.balance -= float(amount)
+        
+        profile.save()
+        return redirect("/")
 
+
+    
+    context = {"profile" : profile , "expenses": expenses}
     return render(request , "home.html",context)
